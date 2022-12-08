5ba50922db1da3dbbd6c1237c2823ec7a02b3e7c
peerberger <peerplamel@gmail.com>
2022-12-08 16:14:16 +0200

    working on android

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { en, he } from './localizations';
+import { useState } from 'react';
+import * as Localization from 'expo-localization';
+import i18n from 'i18n-js';
 
 export default function App() {
+  let [locale, setLocale] = useState(Localization.locale);
+  i18n.fallbacks = true;
+  i18n.translations = { en, he };
+  i18n.locale = locale;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      {locale !== 'en' ? <Button title='English' onPress={() => setLocale('en')} /> : undefined}
+      {locale !== 'he' ? <Button title='Hebrew' onPress={() => setLocale('he')} /> : undefined}
+
+      <Text>{i18n.t('appName')}</Text>
+      <Text>{i18n.t('hello')}</Text>
+
     </View>
   );
 }
---
+++ localizations.js
@@ -0,0 +1,13 @@
+const en = {
+    appName: "POC",
+    hello: "Hello",
+}
+
+const he = {
+    hello: "שלום",
+}
+
+export {
+    en,
+    he,
+}
